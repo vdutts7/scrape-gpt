b5b785127c2989a3079dc8a29c6ccc4595d837ef
vd7 <me@vd7.io>
2024-09-11 16:05:34 -0700

    removed dysfunc code frm  streamlit.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,12 +5,3 @@ import json
 from datetime import datetime
 from scraper import fetch_html_selenium, save_raw_data, format_data, save_formatted_data, calculate_price,html_to_markdown_with_readability, create_dynamic_listing_model,create_listings_container_model
 
-
-if st.session_state.get('perform_scrape'):
-    df, formatted_data, markdown, input_tokens, output_tokens, total_cost, timestamp = st.session_state['results']
-    # Display the DataFrame and other data
-    st.write("Scraped Data:", df)
-    st.sidebar.markdown("## Token Usage")
-    st.sidebar.markdown(f"**Input Tokens:** {input_tokens}")
-    st.sidebar.markdown(f"**Output Tokens:** {output_tokens}")
-    st.sidebar.markdown(f"**Total Cost:** :green-background[***${total_cost:.4f}***]")
